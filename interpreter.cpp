b70a53f613cf613fc66c62732a691c06d13748dc
Nathan Schwantes <nathan.schwantes.180@gmail.com>
2022-11-15 11:35:19 -0800

    LexicalAnalyzer class brought in and run createTokens() for analysis

--- interpreter.cpp
+++ interpreter.cpp
@@ -1,1 +1,11 @@
 #include "interpreter.h"
+#include "lexanalyzer.h"
+
+#include <vector>
+#include <string>
+
+LexicalAnalyzer lex; // bring class to this file
+
+void Interface::interpretCode(std::vector<std::string>& programCode) {
+    lex.createTokens(programCode);
+}
