80a8baf9f7d125e4cec29842da4de2247918dcf5
Nathan Schwantes <nathan.schwantes.180@gmail.com>
2022-11-15 11:40:10 -0800

    2D for loop to iterate through lex.tokenInfo

--- interpreter.cpp
+++ interpreter.cpp
@@ -7,5 +7,12 @@
 LexicalAnalyzer lex; // bring class to this file
 
 void Interface::interpretCode(std::vector<std::string>& programCode) {
+    // run lexicalAnalysis on code for easier interpretation
     lex.createTokens(programCode);
+
+    for (int i = 0; i < lex.tokenInfo.size(); i++) {
+        for (int j = 0; j < lex.tokenInfo[i].size(); j++) {
+
+        }
+    }
 }
