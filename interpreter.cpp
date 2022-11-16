f8761a585b16124ec1527411dac8905d78105b8b
Nathan Schwantes <nathan.schwantes.180@gmail.com>
2022-11-16 12:42:17 -0800

    bugfixes and removeQuotation() finished

--- interpreter.cpp
+++ interpreter.cpp
@@ -10,20 +10,16 @@ LexicalAnalyzer lex; // bring class to this file
 void Interpreter::interpretCode(std::vector<std::string>& programCode) {
     // run lexicalAnalysis on code for easier interpretation
     lex.createTokens(programCode);
-    cout << "TOKEN VECT SIZE" << lex.tokenInfo.size() << endl;
     for (int i = 0; i < lex.tokenInfo.size(); i++) {
-        cout << "fart10" << endl;
         // add current line to tokenLines struct
         lex.tokenLine = lex.tokenInfo[i];
         // if COMMENT do nothing
         if (lex.tokenInfo[i][0].second == categoryType::COMMENT) {
             Interpreter::interpretComment();
-            cout << "fart2" << endl;
         }
         // if PRINT statement then run print function
         else if (lex.tokenInfo[i][0].second == categoryType::KEYWORD && lex.tokenInfo[i][0].first == "print") {
             Interpreter::interpretPrint(lex.tokenLine);
-            cout << "fart3" << endl;
         }
     }
 }
@@ -34,17 +30,26 @@ void Interpreter::interpretComment(void) {
 
 void Interpreter::interpretPrint(LexicalAnalyzer::tokenLineType& programLine) {
     // get iterator for end of programLine
-    int endIter = programLine.size() - 1;
+    size_t endIter = programLine.size() - 1;
     // checks for balanced parenthesis
     if ((programLine[1].second == categoryType::LEFT_PAREN) && (programLine[endIter].second == categoryType::RIGHT_PAREN)) {
         for (int i = 2; i < endIter - 1; i++) {
             if (programLine[i].second == categoryType::STRING_LITERAL) {
-
+                programLine[i].first = Interpreter::removeQuotation(programLine[i].first);
+                cout << programLine[i].first << endl;
             }
         }
     }
 }
 
 string Interpreter::removeQuotation(string& stringLiteral) {
-
+    // removes double quotes
+    if (stringLiteral[0] == '"') {
+        stringLiteral.erase(std::remove(stringLiteral.begin(), stringLiteral.end(), '\"'), stringLiteral.end());
+    }
+    // removes single quotes
+    else if (stringLiteral[0] == '\'') {
+        stringLiteral.erase(std::remove(stringLiteral.begin(), stringLiteral.end(), '\''), stringLiteral.end());
+    }
+    return stringLiteral;
 }
